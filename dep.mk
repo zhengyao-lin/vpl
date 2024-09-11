82af6e51b4fa63bc6f6f20850977a52b6a190100
Zhengyao Lin <zhengyao-lin@outlook.com>
2024-09-11 14:41:13 -0400

    Refactor build scripts and add Vest source code

---
+++ dep.mk
@@ -0,0 +1,53 @@
+##########################################################################
+### Makefile to compile Verus project with external cargo dependencies ###
+##########################################################################
+
+# Uses these variables (for example):
+#   TARGET = Target executable name (e.g. vpl)
+#   SOURCE = All source files used for monitoring changes (e.g. $(wildcard src/*.rs) $(wildcard src/*.pl))
+#   MAIN = Main file passed to Verus (e.g. src/main.rs)
+#   CARGO_DEPS = Rust dependencies added with `cargo add` (e.g. peg clap thiserror tempfile)
+#   VERUS_DEPS = Verus dependency paths (e.g. vest). For each dep in VERUS_DEPS, we expect $(dep).rlib and $(dep).verusdata to exist
+
+.PHONY: debug
+debug: target/debug/$(TARGET)
+
+.PHONY: release
+release: target/release/$(TARGET)
+
+# If $(TARGET) ends in rlib, pass some additional flags to Verus
+ifeq ($(suffix $(TARGET)),.rlib)
+	LIB_FLAGS = --crate-type=lib
+else
+	LIB_FLAGS =
+endif
+
+# Each dependency <dep> in CARGO_DEPS is mapped to verus argument --extern <dep>=target/<release/debug>/deps/lib<dep>-*.rlib
+# Each dependency <dep> in VERUS_DEPS is mapped to verus argument
+#     --extern <dep>=../<dep>/target/<release/debug>/lib<dep>.rlib
+#     --import <dep>=../<dep>/target/<release/debug>/lib<dep>.rlib.verusdata
+target/%/$(TARGET): \
+	$(foreach dep,$(CARGO_DEPS),target/%/lib$(dep).rlib) \
+	$(foreach dep,$(VERUS_DEPS),../$(dep)/target/%/lib$(dep).rlib) \
+	$(SOURCE)
+	
+	mkdir -p target/$*
+	verus $(MAIN) \
+		$(LIB_FLAGS) \
+		-L dependency=target/$*/deps \
+		$(foreach dep,$(CARGO_DEPS),--extern $(dep)=$(firstword $(wildcard target/$*/deps/lib$(dep)-*.rlib))) \
+		$(foreach dep,$(VERUS_DEPS),--extern $(dep)=../$(dep)/target/$*/lib$(dep).rlib --import $(dep)=../$(dep)/target/$*/lib$(dep).rlib.verusdata) \
+		--compile \
+		$(if $(filter release,$*),-C opt-level=3) \
+		-o target/$*/$(TARGET) \
+		--export target/$*/$(TARGET).verusdata
+
+target/debug/lib%.rlib: Cargo.toml
+	cargo build --package=$*
+
+target/release/lib%.rlib: Cargo.toml
+	cargo build --package=$* --release
+
+.PHONY: clean
+clean:
+	cargo clean
